96a2ef81f777149f7ad9588dcc00177b33c7a586
Oleg Balunenko <oleg.balunenko@be-mobile.com>
2017-12-01 02:53:52 +0300

    Add separate checks for host adn guest arguments
    Signed-off-by: Oleg Balunenko <oleg.balunenko@be-mobile.com>

--- main.go
+++ main.go
@@ -16,18 +16,23 @@ func main() {
 	flag.BoolVar(&isHost, "listen", false, "Listens on the specified ip address")
 	flag.Parse()
 
-	if len(os.Args) <= 1 {
-		log.Fatal("Error: ip address not specified")
-	}
-
 	fmt.Println("Length of arguments: ", len(os.Args))
 
 	if isHost {
 		// go run  main.go  -listen <ip>
+		if len(os.Args) <= 2 {
+			log.Fatal("Error: ip address not specified")
+		}
+
 		connIP := os.Args[2]
 		lib.RunHost(connIP)
+
 	} else {
 		// go run main.go  <ip>
+		if len(os.Args) <= 1 {
+			log.Fatal("Error: ip address not specified")
+		}
+
 		connIP := os.Args[1]
 		lib.RunGuest(connIP)
 	}
