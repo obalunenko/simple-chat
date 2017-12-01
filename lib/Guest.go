ca3ac9a9eb3f31ae55ba0176e31b886bf06a8da2
Oleg Balunenko <oleg.balunenko@be-mobile.com>
2017-12-01 15:21:49 +0300

    Implement Guest
    Signed-off-by: Oleg Balunenko <oleg.balunenko@be-mobile.com>

--- lib/Guest.go
+++ lib/Guest.go
@@ -1,6 +1,12 @@
 package lib
 
-import "net"
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+	"os"
+)
 
 // RunGuest takes an argument ip and connects to host with ip
 func RunGuest(ip string) {
@@ -9,6 +15,18 @@ func RunGuest(ip string) {
 	conn, dialErr := net.Dial("tcp", ipAndPort)
 	if dialErr != nil {
 
+		log.Fatal("Error: ", dialErr)
+
 	}
 
+	fmt.Print("Send message: ")
+	reader := bufio.NewReader(os.Stdin)
+
+	message, readErr := reader.ReadString('\n')
+
+	if readErr != nil {
+		log.Fatal("Error: ", readErr)
+	}
+
+	fmt.Fprint(conn, message)
 }
