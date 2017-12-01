6f25b48fe24dd21bdfe3450e659581ab54d1e28f
Oleg Balunenko <oleg.balunenko@be-mobile.com>
2017-12-01 18:23:39 +0300

    Refactoring: Parse non-flag command-line arguments (only ip is expected) and if no arguments exit program
    Signed-off-by: Oleg Balunenko <oleg.balunenko@be-mobile.com>

--- main.go
+++ main.go
@@ -2,9 +2,7 @@ package main
 
 import (
 	"flag"
-	"fmt"
 	"log"
-	"os"
 
 	"github.com/oleg-balunenko/simple-chat/lib"
 )
@@ -15,25 +13,19 @@ func main() {
 
 	flag.BoolVar(&isHost, "listen", false, "Listens on the specified ip address")
 	flag.Parse()
-
-	fmt.Println("Length of arguments: ", len(os.Args))
+	if len(flag.Args()) < 1 {
+		log.Fatal("Error:ip address not specified")
+	}
+	connIP := flag.Args()[1]
 
 	if isHost {
 		// go run  main.go  -listen <ip>
-		if len(os.Args) <= 2 {
-			log.Fatal("Error: ip address not specified")
-		}
 
-		connIP := os.Args[2]
 		lib.RunHost(connIP)
 
 	} else {
 		// go run main.go  <ip>
-		if len(os.Args) <= 1 {
-			log.Fatal("Error: ip address not specified")
-		}
 
-		connIP := os.Args[1]
 		lib.RunGuest(connIP)
 	}
 
