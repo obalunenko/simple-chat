28a2bc7c476f4dfe9cd8c9b9c682353ac6b1ac92
Oleg Balunenko <oleg.balunenko@be-mobile.com>
2017-12-02 22:15:34 +0300

    lib: Implement setName and setIp functions for Client
    Signed-off-by: Oleg Balunenko <oleg.balunenko@be-mobile.com>

--- lib/types.go
+++ lib/types.go
@@ -1,10 +1,41 @@
 package lib
 
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"time"
+)
+
 const port = "8080"
 
 // Client
 type Client struct {
-	IP      string
-	Name    string
-	Message string
+	IP   string
+	Name string
+	Message
+}
+
+// SetName
+func (c *Client) SetName() {
+	fmt.Print("Enter your name: ")
+	setNameReader := bufio.NewReader(os.Stdin)
+	name, setNameErr := setNameReader.ReadString('\n')
+	if setNameErr != nil {
+		log.Fatal("Error: ", setNameErr)
+	}
+	c.Name = name
+
+}
+
+// SetIP
+func (c *Client) SetIP(ip string) {
+	c.IP = ip
+}
+
+// Message
+type Message struct {
+	timestamp time.Time
+	Message   string
 }
