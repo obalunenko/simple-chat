1ea94e7e6f545bfcf3d2b3641fe33070ee18095a
Oleg Balunenko <oleg.balunenko@be-mobile.com>
2017-12-05 14:44:22 +0300

    Implement Messages lib functionality. Receive and Send byte arrays

---
+++ lib/Messages.go
@@ -0,0 +1,31 @@
+package lib
+
+import (
+	"fmt"
+	"log"
+	"net"
+
+	"github.com/oleg-balunenko/simple-chat/lib/chatTypes"
+)
+
+// sendData sends byte array of Client to connection net.Conn
+func sendData(c *chatTypes.Client, conn net.Conn) {
+
+	jsonDataToSend := c.ObjectToJson()
+
+	fmt.Println("Will be send: ", string(jsonDataToSend))
+
+	conn.Write(jsonDataToSend)
+}
+
+// receiveData receives byte array from connection net.Conn
+func receiveData(_, conn net.Conn) (jsonData []byte) {
+
+	_, err := conn.Read(jsonData)
+	if err != nil {
+		log.Fatal("Error: ", err)
+	}
+
+	fmt.Println("Message received", jsonData)
+	return jsonData
+}
