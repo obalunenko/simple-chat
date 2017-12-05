f2bc36d710d073a71429d4a9498c5d430194f04a
Oleg Balunenko <oleg.balunenko@be-mobile.com>
2017-12-05 17:38:39 +0300

    lib/Messages.go: Implement receiveData(); Add info to errors

--- lib/Messages.go
+++ lib/Messages.go
@@ -1,7 +1,6 @@
 package lib
 
 import (
-	"fmt"
 	"log"
 	"net"
 
@@ -11,21 +10,36 @@ import (
 // sendData sends byte array of Client to connection net.Conn
 func sendData(c *chatTypes.Client, conn net.Conn) {
 
-	jsonDataToSend := c.ObjectToJson()
+	jsonDataToSend := make([]byte, 500)
+	jsonDataToSend = c.ObjectToJson()
 
-	fmt.Println("Will be send: ", string(jsonDataToSend))
+	//fmt.Println("Length of []byte object:", len(jsonDataToSend))
+
+	//fmt.Println("Will be send: ", string(jsonDataToSend))
 
 	conn.Write(jsonDataToSend)
+	//fmt.Println("Data sent...")
 }
 
 // receiveData receives byte array from connection net.Conn
-func receiveData(conn net.Conn) (jsonData []byte) {
+func receiveData(conn net.Conn) []byte {
 
-	_, err := conn.Read(jsonData)
-	if err != nil {
-		log.Fatal("Error: ", err)
+	dataReceived := false
+
+	jsonData := make([]byte, 500)
+
+	for !dataReceived {
+		_, err := conn.Read(jsonData)
+		if err != nil {
+			log.Fatal("receiveData(conn net.Conn): Error to conn.Read into jsonData: ", err)
+		}
+
+		if len(jsonData) > 0 {
+			dataReceived = true
+		}
+
 	}
 
-	fmt.Println("Message received", jsonData)
+	//fmt.Println("Data received: ", jsonData)
 	return jsonData
 }
