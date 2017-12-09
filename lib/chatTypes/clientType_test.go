7cd8a899bd7ab6ec9cd0573240e0a2ebd8915515
Oleg Balunenko <oleg.balunenko@be-mobile.com>
2017-12-09 04:10:25 +0300

    lib/chatTypes/ClientType_test.go: add tests

--- lib/chatTypes/clientType_test.go
+++ lib/chatTypes/clientType_test.go
@@ -44,7 +44,7 @@ func TestClient_Port(t *testing.T) {
 
 func TestClient_IP(t *testing.T) {
 
-	Convey("#TestIP()", t, func() {
+	Convey("#TestClient_IP()", t, func() {
 		Convey("Should return IP of Client", func() {
 			expectedResult := testClient.address.ip
 
@@ -55,6 +55,56 @@ func TestClient_IP(t *testing.T) {
 
 }
 
+func TestClient_SetAddress(t *testing.T) {
+	Convey("#TestClient_SetAddress", t, func() {
+		newTestClient := new(Client)
+		Convey("When called with IP 192.192.100.100 as argument", func() {
+
+			newTestClient.SetAddress("192.192.100.100")
+
+			Convey("Port should be set up to 8080", func() {
+				So(newTestClient.address.port, ShouldEqual, "8080")
+			})
+			Convey("IP should be set uo to 192.192.100.100", func() {
+				So(newTestClient.address.ip, ShouldEqual, "192.192.100.100")
+			})
+
+		})
+		Convey("When called with empty IP as argument", func() {
+
+			Convey("Error should be returned", func() {
+				So(newTestClient.SetAddress(""), ShouldBeError)
+			})
+
+		})
+
+	})
+
+}
+
+func TestClient_Address(t *testing.T) {
+	Convey("#TestClient_Address()", t, func() {
+		Convey("When called should return fields of address nested struct for Client object", func() {
+
+			result := testClient.Address()
+
+			Convey("Address should equal to '100.100.100.100:8080'", func() {
+				expectedResult := "100.100.100.100:8080"
+
+				So(result, ShouldEqual, expectedResult)
+			})
+		})
+
+	})
+}
+
+/*func TestClient_Message(t *testing.T) {
+	Convey("#TestCLient_Message()", t, func() {
+		Convey("When called, message should be composed using Client object fields", func() {
+			result := testClient.Message()
+		})
+	})
+}*/
 func TestClient_Name(t *testing.T) {
 	Convey("#TestClient_Name() ", t, func() {
 		Convey(" Should return Name of Client", func() {
@@ -156,4 +206,12 @@ func TestClient_ObjectToJson(t *testing.T) {
 		})
 
 	})
+}
+
+func ExampleClient_Message() {
+
+	testClient.Message()
+	// OUTPUT:
+	// 2017-12-07 - message from TestName: Test Message
+
 }
