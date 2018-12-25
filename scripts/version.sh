55a25f96421396a9463ee8c89a9b8b424534f041
Oleg Balunenko <obalunenko@partners.luno.com>
2018-12-25 17:02:47 +0300

    scripts: update version.sh

--- scripts/version.sh
+++ scripts/version.sh
@@ -21,7 +21,7 @@ function require_clean_work_tree () {
         err=1
     fi
     
-    if [ $err = 1 ]
+    if [[ ${err} = 1 ]]
     then
         echo >&2 "Please commit or stash them."
         exit 1
