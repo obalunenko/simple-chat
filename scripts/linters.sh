c1644dbca9c6e0e31068fc3a6300b69d26f1fef9
Oleg Balunenko <oleg.balunenko@gmail.com>
2019-02-22 15:55:34 +0300

    Update linters.sh

--- scripts/linters.sh
+++ scripts/linters.sh
@@ -2,43 +2,77 @@
 
 function vet(){
     echo "vet project..."
-    go vet ./... | grep -v "vendor"
+    go vet $(go list ./... | grep -v /vendor/)
     echo ""
 }
 
 function fmt(){
-go fmt $(go list ./... | grep -v /vendor/)
-
+    go fmt $(go list ./... | grep -v /vendor/)
+    
 }
 function golangci(){
-    echo "golang-ci lint..."
-    golangci-lint run ./...
+    echo "golang-ci linter running..."
+    if [ -f "$(go env GOPATH)/bin/golangci-lint" ] || [ -f "/usr/local/bin/golangci-lint" ]; then
+        golangci-lint run ./...
+    else
+        printf "Cannot check golang-ci, please run:
+        curl -sfL https://install.goreleaser.com/github.com/golangci/golangci-lint.sh | sh -s -- -b $(go env GOPATH)/bin v1.12.5 \n"
+        exit 1
+    fi
     echo ""
 }
 
 function go-lint(){
-    echo "golint..."
-    declare -a lints=$(golint ./... | grep -v "vendor")
-    if [[ $lints ]]; then
-        echo "fix it:"
-        for l in "${lints[@]}"
-        do
-            echo "$l"
+    echo "golint linter running..."
+    if [ -f "$(go env GOPATH)/bin/golint" ]; then
+        declare -a lints=$(golint $(go list ./... | grep -v /vendor/))
+        if [[ $lints ]]; then
+            echo "fix it:"
+            for l in "${lints[@]}"
+            do
+                echo "$l"
+                
+            done
+            exit 1
             
-        done
-        exit 1
-        
+        else
+            echo "code is ok"
+            echo $lints
+        fi
     else
-        echo "code is ok"
-        echo $lints
+        printf "Cannot check golint, please run:
+        go get -u -v golang.org/x/lint/golint \n"
+        exit 1
     fi
+    
     echo ""
 }
 
 function go-group()
 {
-    echo "gogroup..."
-    gogroup -order std,other,prefix=github.com/oleg-balunenko/  $(find . -type f -name "*.go" | grep -v "vendor/")
+    echo "gogroup imports check is running..."
+    if [ -f "$(go env GOPATH)/bin/gogroup" ]; then
+        
+        declare -a lints=$(gogroup -order std,other,prefix=github.com/oleg-balunenko/ $(find . -type f -name "*.go" | grep -v "vendor/"))
+        if [[ $lints ]]; then
+            echo "fix it:"
+            for l in "${lints[@]}"
+            do
+                echo "$l"
+                
+            done
+            exit 1
+            
+        else
+            echo "code is ok"
+            echo $lints
+        fi
+        
+    else
+        printf "!!!! Cannot check import order, please run:
+        go get -u -v github.com/Bubblyworld/gogroup\n"
+        exit 1
+    fi
     echo ""
 }
 
